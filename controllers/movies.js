e3d68a5c68054f767ad1724dca342c8d8406386a
Vova Pol <ya-chai777@yandex.ru>
2023-02-22 17:22:39 +0300

    написал контроллеры для фильмов

--- controllers/movies.js
+++ controllers/movies.js
@@ -1,8 +1,84 @@
-function getSavedMovies(req, res, next) {}
+const Movie = require('../models/movie');
+const { Error } = require('mongoose');
+const NotFoundErr = require('../errors/not-found');
+const BadRequestErr = require('../errors/bad-request');
 
-function postSavedMovie(req, res, next) {}
+function getSavedMovies(req, res, next) {
+  Movie.find()
+    .populate('owner')
+    .then((data) => {
+      if (data) {
+        res.send({ data });
+      } else {
+        next(new NotFoundErr('Не найдено ни одного фильма'));
+      }
+    })
+    .catch(next);
+}
 
-function deleteSavedMovie(req, res, next) {}
+async function postSavedMovie(req, res, next) {
+  try {
+    const {
+      country,
+      director,
+      duration,
+      year,
+      description,
+      image,
+      trailerLink,
+      nameRu,
+      nameEn,
+      thumbnail,
+      movieId,
+    } = req.body;
+
+    const savedMovie = await Movie.create({
+      country,
+      director,
+      duration,
+      year,
+      description,
+      image,
+      trailerLink,
+      nameRu,
+      nameEn,
+      thumbnail,
+      movieId,
+      owner: req.user._id,
+    });
+
+    await savedMovie.populate('owner');
+    res.send({ data: savedMovie });
+  } catch (err) {
+    if (err instanceof Error.ValidationError) {
+      next(
+        new BadRequestErr('Переданы некорректные данные при сохранении фильма'),
+      );
+    } else {
+      next(err);
+    }
+  }
+}
+
+function deleteSavedMovie(req, res, next) {
+  const { movieId } = req.params;
+
+  Movie.findByIdAndRemove(movieId)
+    .then((data) => {
+      if (!data) {
+        next(new NotFoundErr('Фильм с указанным _id не найден'));
+      } else {
+        res.send({ data });
+      }
+    })
+    .catch((err) => {
+      if (err instanceof Error.CastError) {
+        next(new BadRequestErr('Передан некорректный _id фильма'));
+      } else {
+        next(err);
+      }
+    });
+}
 
 module.exports = {
   getSavedMovies,
