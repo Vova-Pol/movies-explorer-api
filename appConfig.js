48d209efe31db70f2430c004393e43325df27cf6
Vova Pol <ya-chai777@yandex.ru>
2023-02-25 17:40:32 +0300

    изменил название базы данных

--- appConfig.js
+++ appConfig.js
@@ -4,8 +4,11 @@ const { NODE_ENV = 'dev' } = process.env;
 const { JWT_SECRET = 'dev-secret-key' } = process.env;
 
 const { PORT = 3000 } = process.env;
-const { DB_URL = 'mongodb://localhost:27017/movies-explorer' } = process.env;
+const { DB_URL = 'mongodb://localhost:27017/bitfilmsdb' } = process.env;
 
 module.exports = {
-  NODE_ENV, JWT_SECRET, PORT, DB_URL,
+  NODE_ENV,
+  JWT_SECRET,
+  PORT,
+  DB_URL,
 };
